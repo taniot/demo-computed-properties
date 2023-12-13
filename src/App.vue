8e0fa5ea2c44c765049f91f8598d927aa45821c6
taniot <gaetano.frascolla@gmail.com>
2023-12-13 14:55:47 +0100

    feat: demo computed

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,42 @@ export default {
   data() {
     return {
       text: 'Template VueJS',
+      firstName: 'Gaetano',
+      middleName: '',
+      lastName: 'Frascolla',
+      age: 40,
     };
+  },
+  methods: {
+    // getFullName() {
+    //   console.log('sono stato invocato');
+    //   return `${this.firstName} ${this.middleName} ${this.lastName}`;
+    // },
+  },
+  computed: {
+    getFullName() {
+      console.log('sono stato invocato');
+      return `${this.firstName} ${this.middleName} ${this.lastName}`;
+    },
   },
 };
 </script>
 
 <template>
-  <h1>{{ text }}</h1>
+  <h1>
+    {{ getFullName }}
+  </h1>
+
+  <div>
+    <label>FirstName</label>
+    <input type="text" v-model="firstName" />
+    <label>middleName</label>
+    <input type="text" v-model="middleName" />
+    <label>lastName</label>
+    <input type="text" v-model="lastName" />
+    <label>age</label>
+    <input type="number" v-model="age" />
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
